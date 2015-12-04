c05c7d34ed5970fdbd3839409e0aa0f08f6dc730
Adam Eriksson <adam@hotmail.se>
2015-12-04 01:17:02 +0100

    La till timer för graffik

--- ConsoleApplication2/ConsoleApplication2/Form1.cs
+++ ConsoleApplication2/ConsoleApplication2/Form1.cs
@@ -12,9 +12,27 @@ namespace ConsoleApplication2
 {
     public partial class Form1 : Form
     {
+        private Timer timer;
+        private int x;
+        private bool turn;
+        private Image borderImage, cardImage;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            borderImage = Image.FromFile(Environment.CurrentDirectory + "\\basic_card_blood.png");
+            
+
+            timer = new Timer(new System.ComponentModel.Container());
+
+            x = 0;
+            turn = false;
+            timer.Enabled = true;
+            timer.Interval = 13;
+            timer.Tick += new System.EventHandler(this.timer_tic);
+
             DrawIt();
         }
 
@@ -24,15 +42,15 @@ namespace ConsoleApplication2
 
             Logic.MonsterCard monster = new Logic.MonsterCard("Dragon", "spits fire", "Dragon.jpg", 5);
 
-            DrawCard(graphics, monster, 50, 50, 2);
+            DrawCard(graphics, monster, 0, 0, 1.4f);
+            DrawCard(graphics, monster, 300, 150, 0.4f);
 
         }
 
         private void DrawCard(System.Drawing.Graphics graphics, Logic.Card card, float x, float y, float scale)
         {
             Console.WriteLine(Environment.CurrentDirectory + "\\" + card.Image);
-            Image borderImage = Image.FromFile(Environment.CurrentDirectory + "\\basic_card_blood.png");
-            Image cardImage = Image.FromFile(Environment.CurrentDirectory + "\\" + card.Image);
+            cardImage = Image.FromFile(Environment.CurrentDirectory + "\\" + card.Image);
 
             graphics.DrawImage(borderImage, x, y, 200*scale, 320*scale);
             graphics.DrawImage(cardImage, x+25*scale, y+25*scale, 150*scale, 170*scale);
@@ -49,6 +67,29 @@ namespace ConsoleApplication2
         private void button1_Click(object sender, EventArgs e)
         {
             DrawIt();
+        }
+
+        private void timer_tic(Object sender, EventArgs e)
+        {
+            if(turn)
+            {
+                x--;
+                if (x == 0)
+                    turn = false;
+            } else
+            {
+                x++;
+                if (x == 50)
+                {
+                    turn = true;
+                }
+            }
+
+
+            System.Drawing.Graphics graphics = this.CreateGraphics();
+            Logic.MonsterCard monster = new Logic.MonsterCard("Dragon", "spits fire", "Dragon.jpg", 5);
+            DrawCard(graphics, monster, x, 0, 1.4f);
+            //dosuff
         }
     }
 }
